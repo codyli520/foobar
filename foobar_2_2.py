a0c299175ad60b1f52cf49ca2c204e16d357bab3
Cody <codyli520@gmail.com>
2017-10-09 17:55:19 -0700

    failed attempt

--- foobar_2_2.py
+++ foobar_2_2.py
@@ -2,26 +2,20 @@ def answer(h,q):
 	num_node = 2**h - 1
 	num_leaf = 2**(h-1)
 
-	tree_dict = {}
-	nodes = [num_node]
-	level = 1
-	while len(nodes)>0:
-		node = nodes.pop(0)
-		left_child = node/2
-		right_child = node - 1
+	nodes = [x+1 for x in range(num_node)]
 
-		if left_child>0 and right_child>0:
-			tree_dict[node] = {"l_child":left_child,"r_child":right_child}
-			level+=1
-			if level<h:
-				nodes.append(left_child)
-				nodes.append(right_child)
+	tree= {}
+
+	while len(nodes)>2:
+		node = nodes.pop()
+		left_child = nodes.pop(len(nodes)/2-1)
+		right_child = nodes.pop(len(nodes)-1)
+		tree[node] = {"left":left_child,"right":right_child}
 		
 	
-	return tree_dict
-
+	return tree
 
 
 if __name__ == "__main__":
 
-	print answer(4,10)
+	print answer(3,10)
