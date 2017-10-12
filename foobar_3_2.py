e326f8c5d8b261b1d2910a50e586fee4726fbddb
Longfei Li <codyli520@gmail.com>
2017-10-12 08:53:24 -0700

    level 3-2 complete

---
+++ foobar_3_2.py
@@ -0,0 +1,18 @@
+def answer(l):
+    if len(l) < 3:
+        return 0
+
+    count = 0
+    i_ls = []
+    k_ls = []
+    for j in range(1, len(l) - 1):
+        for i in range(j):
+            if l[j] % l[i] == 0:
+                i_ls.append(l[i])
+        for k in range(j+1,len(l)):
+            if l[k] % l[j] == 0:
+                k_ls.append(l[k])
+        count += len(i_ls)*len(k_ls)
+        i_ls = []
+        k_ls = []
+    return count
